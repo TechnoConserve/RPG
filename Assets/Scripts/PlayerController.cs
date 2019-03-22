672fb300030fad9e5c0f03f2847d9cc4d406660d
Avery Uslaner <uslaner.avery@gmail.com>
2019-03-22 15:01:01 -0600

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    public Interactable focus;
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        // If we press left mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                // Move our player to what we hit
+                motor.MoveToPoint(hit.point);
+
+                // Stop focusing any objects
+                RemoveFocus();
+            }
+        }
+
+        // if we click right mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Check if we hit an interactable
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                // If so, set it as our focus
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocused(transform);
+    }
+}
